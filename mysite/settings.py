593570db8c4dc9299f3c8efea5894aea8bce98d1
syohanaferreira <syohanaferreiram@gmail.com>
2019-03-15 13:52:27 +0100

    añadir url

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3qw@qxj^@7ts+zzwtxidb*&$evp$g#cgzymjs$0*msrg$k(184'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com', 'http://syohanaferreira.pythonanywhere.com/']
 
 
 # Application definition
